ac339943473e27822fe4a022a5de8b900e7103e8
Sofia Brantley <skbran@unc.edu>
2025-04-09 16:00:27 -0400

    EX04- River Simulation

--- exercises/ex04/river.py
+++ exercises/ex04/river.py
@@ -3,9 +3,14 @@
 from exercises.EX04.fish import Fish
 from exercises.EX04.bear import Bear
 
+
 class River:
     
-    def __init__(self, num_fish: int, num_bears:int):
+    day: int  # tells day of river lifecycle you are modeling
+    bears: list[Bear]
+    fish: list[Fish]
+    
+    def __init__(self, num_fish: int, num_bears: int):
         """New River with num_fish Fish and num_bears Bears"""
         self.day: int = 0
         self.fish: list[Fish] = []
@@ -17,21 +22,56 @@ class River:
             self.bears.append(Bear())
 
     def check_ages(self):
+        surviving_bears: list[Bear] = self.bears
+        surviving_fish: list[Fish] = self.fish
+        for fish in self.fish:
+            if fish.age <= 3:  # need help on this section/syntax for referencing age for other classes
+                surviving_fish.append(fish)
+        for bear in self.bears:
+            if bear.age <= 5:
+                surviving_bears.append(bear)
+        self.bears = surviving_bears
+        self.fish = surviving_fish
         return None
-
+    
+    def remove_fish(self, amount: int) -> None:
+        index = 0
+        while index < amount:
+            self.fish.pop(index)
+            index += 1
+    
     def bears_eating(self):
+        for each_bear in self.bears:
+            if len(self.fish) >= 5:
+                self.remove_fish(3)
+                each_bear.eat(3) 
         return None
     
     def check_hunger(self):
+        fat_bears: list[Bear] = self.bears
+        for each_bear in fat_bears:
+            if each_bear.hunger_score >= 0:  # syntax for accessing the hunger_score?
+                fat_bears.append(each_bear)
+        self.bears = fat_bears
         return None
         
     def repopulate_fish(self):
+        n = (len(self.fish) // 2) * 4
+        while n > 0:
+            self.fish.append(Fish())
         return None
     
     def repopulate_bears(self):
+        n = len(self.bears) // 2
+        while n > 0:
+            self.bears.append(Bear())
+            n -= 1
         return None
     
     def view_river(self):
+        print(f"~~~ Day {self.day}: ~~~")
+        print(f"Fish population: {len(self.fish)}")
+        print(f"Bear population: {len(self.bears)}")
         return None
             
     def one_river_day(self):
@@ -56,4 +96,7 @@ class River:
         self.repopulate_bears()
         # Visualize River
         self.view_river()
+
+    def one_river_week(self):
+        return None
             
--- exercises/ex04/river_simulation.py
+++ exercises/ex04/river_simulation.py
@@ -1,1 +1,5 @@
+from exercises.EX04.river import River
 
+my_river: River = River(10, 2)  # River object called my_river with 10 Fish and 2 Bears
+my_river.view_river()
+my_river.one_river_week()
